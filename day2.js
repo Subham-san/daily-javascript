8bff88a4e02610578a3ef7e7e98be23fa2527e3f
Subham Nayak <subham9176@gmail.com>
2026-08-07 14:17:49 +0530

    Learning = variables !

---
+++ day2.js
@@ -0,0 +1,16 @@
+// variables = "this are container which stores values"
+// numbers = "used to define quantity , doing calculation"
+// strings = "this is list of characters"
+// boolean = "which is either true or false"
+
+//strings:
+let name = "Subham";
+document.getElementById("p2").textContent = `I am ${name} this is string`;
+
+//number: 
+let x = 19;
+document.getElementById("p1").textContent = `I am ${19} years old and this is number`;
+
+//booleans: 
+let online= true;
+document.getElementById("p3").textContent = `I am a Web developer ${online} and this is boolean`;
