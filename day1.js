9a0af3b0d9ef9b3d3413f20d3d78136df8ff5466
Subham Nayak <subham9176@gmail.com>
2026-08-07 13:29:48 +0530

    daily java script

---
+++ day1.js
@@ -0,0 +1,15 @@
+// This is basic of java script
+/*
+console.log("hello Subham");
+console.log("i am 19 year old");
+*/
+// this is for putting pop-up alerts to webpages
+/*
+window.alert("this is alert");
+window.alert("this is website");
+*/
+
+// this is day 1 of starting java script
+
+document.getElementById("hi").textContent="Hello";
+document.getElementById("p1").textContent="This is Subham";
