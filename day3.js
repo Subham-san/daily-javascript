d99901e2fdcba9790bda49c690f53c3768ab7916
Subham Nayak <subham9176@gmail.com>
2026-08-08 13:22:02 +0530

    Arithmetics operators

---
+++ day3.js
@@ -0,0 +1,31 @@
+// Arithmetic operators ! = Oprands(value , variables , etc. )
+//operators (+,-,/,*,//,**)
+
+//ex :- 11=x+5;
+
+let students= 30;               // output
+
+//students = students + 1;       31
+
+//students = students - 1;       29
+
+//students = students * 2;       60
+
+//students = students / 2;       15
+
+//students = students ** 2;      900
+
+//students = students % 2;       0
+
+//students++;                      31
+//students--;                      29
+
+/* Operators precedence 
+  1. parenthesis
+  2. exponents
+  3. multiplication and division modules
+  4. addition and subs.
+*/
+
+let result = 6/2**(2+5);
+console.log(result);             0.046875
