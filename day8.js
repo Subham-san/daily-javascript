a9bef1e92583b35fd3adb63c299a61c8a42bbf00
Subham Nayak <subham9176@gmail.com>
2026-08-12 22:14:55 +0530

    Learn About Math functions : like sqrt , round , floor , etc

---
+++ day8.js
@@ -0,0 +1,21 @@
+// Math : It was an build-in object that previous a collection of properties and method . 
+// for ex : - 
+
+/* console.log(Math.PI); 
+ output is in console = 3.141592 ...... */
+
+/* console.log(Math.E); 
+ output is in console = 2.7182...... */
+
+ let x = 3.1;
+ let y = 2;
+ let z;
+
+ // there are some other maths collections are 
+
+ /* z = Math.round(x);
+ output is in console = 3
+ z = Math.floor(x);
+ z = Math.cell(x);
+ z = Math.sqrt(x);
+ z = Math.pow*(x);*/
